897de35317520631e179756605c9c5c7753accd2
jimmy <foodsksms@gmail.com>
2018-02-19 21:23:21 +0900

    2018-2-19 baseball finish

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -8,19 +8,26 @@ public class Run {
         System.out.println("입력한 수 : " + inputNumber);
 
         Operator operator = new Operator();
+        // input area
         ArrayList<Integer> inputSplit = operator.splitInt(inputNumber);
         operator.printSplitInt(inputSplit);
 
         System.out.println();
-
+        // random generate area
         ArrayList<Integer> shuffle =  GenerateRandom.shuffle(GenerateRandom.numbers);
         ArrayList<Integer> resultNumbers = GenerateRandom.resultNumber(shuffle);
         GenerateRandom.printResultNumbers(resultNumbers);
         System.out.println();
+
+        // result area
         int countNumber =operator.findStrike(inputSplit , shuffle);
         operator.resultStrike(countNumber);
 
         int countBall = operator.findBall(inputSplit, shuffle);
         operator.resultBall(countBall);
+
+        int countStrike = operator.findStrike(inputSplit, resultNumbers);
+
+
     }
 }
